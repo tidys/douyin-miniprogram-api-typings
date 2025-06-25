b9d8bda8e3f455c2c628fd9ae1892d0203d2bf4c
xu_yanfeng <xu_yanfeng@126.com>
2025-06-25 10:02:53 +0800

    ShowResult 变量

--- types/index.d.ts
+++ types/index.d.ts
@@ -8,8 +8,18 @@ declare const tt: DouyinMinigame.TT;
 interface Window {
   /**将tt声明为Window变量 */
   tt: DouyinMinigame.TT;
+
+  ShowResult: DouyinMinigame.onShowObj | undefined;
 }
 
 declare global {
   const tt: DouyinMinigame.TT;
+  /**
+   * 安装插件后，根据抖音的官方对接要求，会自动在game.js插入以下代码
+   * ```typescript
+   * tt.onShow(res=>window['ShowResult'] = res)
+   * ```
+   * 后续在侧边栏逻辑中可以直接使用该变量
+   */
+  const ShowResult: DouyinMinigame.onShowObj | undefined;
 }
